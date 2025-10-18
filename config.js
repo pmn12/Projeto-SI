f019e90e4d65f2d204417ea4c5bcb7c6f79c4366
Paulo Messias <114231448+pmn12@users.noreply.github.com>
2025-10-17 21:50:46 -0300

    Add global configuration constants in config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// --- CONFIGURAÇÕES GLOBAIS ---
+const COLS = 40;
+const ROWS = 30;
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 600;
+
+// Constantes dos tipos de terreno
+const TERRAIN_LOW_COST = 0;   // Areia
+const TERRAIN_MEDIUM_COST = 1; // Atoleiro
+const TERRAIN_HIGH_COST = 2;  // Água
+const OBSTACLE = 3;           // Obstáculo
